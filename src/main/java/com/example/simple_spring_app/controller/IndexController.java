6f55a9e2323091b1c84e6cd2f5b1bb444353af91
Yosef-Perelman <yosefper@gmail.com>
2024-06-10 11:05:53 +0300

    Added a home page.

--- src/main/java/com/example/simple_spring_app/controller/IndexController.java
+++ src/main/java/com/example/simple_spring_app/controller/IndexController.java
@@ -6,9 +6,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class IndexController {
 
-    @GetMapping("/")
-    public String index() {
-        return "Greetings from Spring Boot!";
-    }
+//    @GetMapping("/")
+//    public String index() {
+//        return "Greetings from Spring Boot!";
+//    }
 
 }
