55c299408b695a3be8384eb7a83bf2b2aa2c1dfd
Yosef-Perelman <yosefper@gmail.com>
2024-06-10 11:54:45 +0300

    Added add.html file.

--- src/main/java/com/example/simple_spring_app/controller/IndexController.java
+++ src/main/java/com/example/simple_spring_app/controller/IndexController.java
@@ -1,20 +1,21 @@
 package com.example.simple_spring_app.controller;
 
 import com.example.simple_spring_app.model.Job;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class IndexController {
 
-//    @GetMapping("/add-job")
-//    public String add_job() {
-//        return "Greetings from Spring Boot!";
-//    }
-//
+    @GetMapping("/add")
+    public String add_job() {
+        return "add";
+    }
+
 //    @PostMapping("/greeting")
 //    public String greetingSubmit(@ModelAttribute Job greeting, Model model) {
 //        model.addAttribute("greeting", greeting);
